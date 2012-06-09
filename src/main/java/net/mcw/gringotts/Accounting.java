7cec2c6ff874589373259e8a50002c592a626f88
squidicuzz <jono324@gmail.com>
2012-06-09 12:19:27 -0400

    Fixed git user derp

--- src/main/java/net/mcw/gringotts/Accounting.java
+++ src/main/java/net/mcw/gringotts/Accounting.java
@@ -2,7 +2,9 @@ package net.mcw.gringotts;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -18,6 +20,8 @@ import org.bukkit.event.block.SignChangeEvent;
  *
  */
 public class Accounting implements Listener {
+	
+	Logger log = Bukkit.getServer().getLogger();
 	
 	public Map<AccountHolder, Account> accounts = new HashMap<AccountHolder, Account>();
 	public Map<AccountHolder, AccountChest> chests = new HashMap<AccountHolder, AccountChest>();
@@ -42,13 +46,15 @@ public class Accounting implements Listener {
 			// TODO faction vault creation
 		} else return; // not for us!
 		
+		log.info("Vault created");
+		
 		Block chestBlock = event.getBlock().getRelative(BlockFace.DOWN);
 		if (chestBlock.getType() == Material.CHEST) {
 			
 			Account account = getOrCreateAccount(chestOwner);
 			
 			// create account chest
-			Chest chest = (Chest)chestBlock;
+			Chest chest = (Chest)chestBlock.getState();
 			AccountChest accountChest = new AccountChest(chest, account);
 			chests.put(chestOwner, accountChest);
 			//FIXME this will probably not work correctly with double chests yet
