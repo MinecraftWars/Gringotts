f912a17fa7a611711f9f48e25bae58d45fe9796e
jast <jast@zno.de>
2012-06-10 13:11:44 +0200

    rename gCommand

---
+++ src/main/java/net/mcw/gringotts/Commands.java
@@ -0,0 +1,34 @@
+package net.mcw.gringotts;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+class Commands implements CommandExecutor  {
+	Logger log = Bukkit.getServer().getLogger();
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		log.info("Command Received");
+		
+		Player player;
+		if (sender instanceof Player) {
+			player = (Player)sender;
+		} else {
+			sender.sendMessage("this command can only be run by a player");
+			return false; // for now, no console commands
+		}
+
+		if(cmd.getName().equalsIgnoreCase("balance")){
+			AccountHolder accountOwner = new PlayerAccountHolder(player);
+			Account account = Gringotts.accounting.getAccount(accountOwner);
+			accountOwner.sendMessage("Your current balance: " + account.balance());
+			return true;
+		}
+		
+		return false; 
+	}
+}
