4181bf2148a0a005d2a5f5c9e8996d5b072fafd0
jast <jast@zno.de>
2012-08-13 23:49:59 +0200

    preparation for sql persistence

---
+++ src/main/java/org/gestern/gringotts/DAO.java
@@ -0,0 +1,27 @@
+package org.gestern.gringotts;
+
+import java.util.Set;
+
+public class DAO {
+
+    /**
+     * Save an account chest to date storage.
+     * @param chest
+     * @return
+     */
+    public boolean saveAccountChest(AccountChest chest) {
+        return false;
+    }
+    
+    public boolean saveAccount(Account account) {
+        return false;
+    }
+    
+    public Account getAccount(String id) {
+        return null;
+    }
+    
+    public Set<Account> getAccounts() {
+        return null;
+    }
+}
