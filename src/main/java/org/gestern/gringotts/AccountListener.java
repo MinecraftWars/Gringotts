6757c0a573fa968416e89fa80d8ee8f7cbb53595
U-BrokenGlass18\Chiori Greene <Chiori Greene@BrokenGlass18.(none)>
2012-10-30 20:54:41 -0500

    Added try/catch to player.GetEnterChest() to make it Minecraft 1.2.5 compatible

--- src/main/java/org/gestern/gringotts/AccountListener.java
+++ src/main/java/org/gestern/gringotts/AccountListener.java
@@ -133,7 +133,7 @@ public class AccountListener implements Listener {
         for (AccountChest chest : dao.getChests()) {
         	if ( loc.equals(chest.sign.getBlock().getLocation()) ) {
         		//chest.destroy();
-        		// NOTE: Removed because breakNaturally() was causing a double call of this event which ends in a intermittent Internal Server Error on Tekkit 3.1.2 
+        		// NOTE: Removed because breakNaturally() was causing a double call of this event which ends in a intermittent Internal Server Error on Tekkit 3.1.2
         		dao.destroyAccountChest(chest);
         		Account account = chest.getAccount();
         		account.owner.sendMessage("Vault broken. New balance is " + account.balance());
