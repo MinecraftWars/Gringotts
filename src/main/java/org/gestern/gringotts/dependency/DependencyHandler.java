3543c55a604454be0c5a683e1c6476a1251f2fd4
Justin Kaeser <jast@zno.de>
2012-12-24 17:41:47 +0100

    documentation update

--- src/main/java/org/gestern/gringotts/dependency/DependencyHandler.java
+++ src/main/java/org/gestern/gringotts/dependency/DependencyHandler.java
@@ -2,6 +2,7 @@ package org.gestern.gringotts.dependency;
 
 /**
  * A DependencyHandler contains methods that deal with a specific Bukkit plugin dependency.
+ * All interactions with any other plugin's API should happen through a handler.
  * 
  * @author jast
  *
