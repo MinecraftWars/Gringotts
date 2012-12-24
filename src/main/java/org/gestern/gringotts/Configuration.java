f7140b670a9c580b804cb5cf1ac2c190798c95c2
Justin Kaeser <jast@zno.de>
2012-12-24 17:40:10 +0100

    basic configuration setup for multiple denominations

--- src/main/java/org/gestern/gringotts/Configuration.java
+++ src/main/java/org/gestern/gringotts/Configuration.java
@@ -1,9 +1,5 @@
 package org.gestern.gringotts;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -51,44 +47,38 @@ public enum Configuration {
      * @param savedConf
      */
     public void readConfig(FileConfiguration savedConfig) {
+    	
         String version = Bukkit.getBukkitVersion();
+
+        
         if (Util.versionAtLeast(version, "1.3.1")) {
         	log.info("[Gringotts] Found Bukkit version: "+version+". All features enabled.");
     		
-    		String currencyNameSingular = savedConfig.getString("currency.name.singular", "Emerald");
-            String currencyNamePlural = savedConfig.getString("currency.name.plural", currencyNameSingular+"s");
-            currency = new Currency(currencyNameSingular, currencyNamePlural);
-            
-        	// legacy currency config, overrides defaults if available
-        	int currencyType = savedConfig.getInt("currency.type",-1);
-        	if (currencyType > 0) {
-        		byte currencyDataValue = (byte)savedConfig.getInt("currency.datavalue", 0);
-        		ItemStack legacyCurrency = new ItemStack(currencyType, 0, (short)0, currencyDataValue);
-        		Denomination denom = new Denomination(legacyCurrency, 1);
-        		currency.addDenomination(denom);
-        	} else {
-        		// regular currency configuration (multi-denomination)
-        		List<String> denoms = savedConfig.getStringList("currency.denominations");
-        		savedConfig.getConfigurationSection("currency.denomination").getKeys(false);
-        		
-        		Map<String,Integer> defaultDenoms = new HashMap<String, Integer>();
-            	defaultDenoms.put("388",1); // emerald
-            	defaultDenoms.put("133",0); // emerald block
-            	ConfigurationSection denomSection = savedConfig.getConfigurationSection("currency.denominations");
-            	parseCurrency(denomSection);
-        	}
+        	config.usevaultEnderchest = savedConfig.getBoolean("usevault.enderchest", true);
             
         } else {
-        	log.info("[Gringotts] Found Bukkit version: "+version+". Disabling 1.3 features.");
-        	
-        	int currencyType = savedConfig.getInt("currency.type", 266);
-            byte currencyDataValue = (byte)savedConfig.getInt("currency.datavalue", 0);
-//    		currency = new ItemStack(currencyType, 0, (short)0, currencyDataValue);
-    		config.currencyNameSingular = savedConfig.getString("currency.name.singular", "Gold Ingot");
-            config.currencyNamePlural = savedConfig.getString("currency.name.plural", config.currencyNameSingular+"s");
+        	log.info("[Gringotts] Found Bukkit version: "+version+". Disabling 1.3+ features.");
         	
             config.usevaultEnderchest = false;
         }
+        
+        String currencyNameSingular, currencyNamePlural;
+    	currencyNameSingular = savedConfig.getString("currency.name.singular", "Emerald");
+        currencyNamePlural = savedConfig.getString("currency.name.plural", currencyNameSingular+"s");
+        currency = new Currency(currencyNameSingular, currencyNamePlural);
+        
+    	// legacy currency config, overrides defaults if available
+    	int currencyType = savedConfig.getInt("currency.type",-1);
+    	if (currencyType > 0) {
+    		byte currencyDataValue = (byte)savedConfig.getInt("currency.datavalue", 0);
+    		ItemStack legacyCurrency = new ItemStack(currencyType, 0, (short)0, currencyDataValue);
+    		Denomination denom = new Denomination(legacyCurrency, 1);
+    		currency.addDenomination(denom);
+    	} else {
+    		// regular currency configuration (multi-denomination)
+    		ConfigurationSection denomSection = savedConfig.getConfigurationSection("currency.denominations");
+        	parseCurrency(denomSection);
+    	}
         
         
         config.currencyFractional = savedConfig.getBoolean("currency.fractional", true);
@@ -100,9 +90,11 @@ public enum Configuration {
     }
     
     /**
-     * Parse a multi-denomination currency from configuration. A currency definition is a ma
-     * @param denoms
-     * @return
+     * Parse a multi-denomination currency from configuration.
+     * A currency definition consists of a map of demoninations to value. 
+     * A denomination type is defined either as the item id, 
+     * or a semicolon-separated string of item id; damage value; data value
+     * @param denomSection config section containaining denomination definition
      */
     private void parseCurrency(ConfigurationSection denomSection) {
     	
@@ -129,19 +121,5 @@ public enum Configuration {
 		}
     	
     }
-
-//    public void saveConfig(FileConfiguration savedConfig) {
-////        savedConfig.set("currency.type", config.currency.getTypeId());
-////        savedConfig.set("currency.datavalue", config.currency.getData().getData());
-//        savedConfig.set("currency.name.singular", config.currencyNameSingular);
-//        savedConfig.set("currency.name.plural", config.currencyNamePlural);
-//        savedConfig.set("currency.fractional", config.currencyFractional);
-//
-//        savedConfig.set("transactiontax.flat", config.transactionTaxFlat);
-//        savedConfig.set("transactiontax.rate", config.transactionTaxRate);
-//        savedConfig.set("usevault.container", config.usevaultContainer);
-//        savedConfig.set("usevault.enderchest", config.usevaultContainer);
-//    }
-
     
 }
