7e126d81f7b513518043d0bb44389d283d5d0cd1
Justin Kaeser <jast@zno.de>
2012-12-25 22:06:16 +0100

    added permissions enum

---
+++ src/main/java/org/gestern/gringotts/Permissions.java
@@ -0,0 +1,30 @@
+package org.gestern.gringotts;
+
+import org.bukkit.entity.Player;
+
+public enum Permissions {
+	usevault_inventory("gringotts.usevault.inventory"),
+	usevault_enderchest("gringotts.usevault.enderchest"),
+	
+	createvault_player("gringotts.createvault.player"),
+	createvault_faction("gringotts.createvault.faction"),
+	createvault_town("gringotts.createvault.town"),
+	createvault_nation("gringotts.createvault.nation"),
+	
+	transfer("gringotts.transfer")
+	;
+	
+	public final String node;
+	Permissions(String node) {
+		this.node = node;
+	}
+	
+	/**
+	 * Check if a player has this permission.
+	 * @param player
+	 * @return
+	 */
+	public boolean allowed(Player player) {
+		return player.hasPermission(this.node);
+	}
+}
