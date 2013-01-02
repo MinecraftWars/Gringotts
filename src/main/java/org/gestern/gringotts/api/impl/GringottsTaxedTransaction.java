0f75b43af996c143a7f0d22c5fc98950029a6a6f
Justin Kaeser <jast@zno.de>
2013-01-02 19:41:53 +0100

    implement transaction api

---
+++ src/main/java/org/gestern/gringotts/api/impl/GringottsTaxedTransaction.java
@@ -0,0 +1,47 @@
+package org.gestern.gringotts.api.impl;
+
+import static org.gestern.gringotts.api.TransactionResult.*;
+
+import org.gestern.gringotts.api.Account;
+import org.gestern.gringotts.api.TaxedTransaction;
+import org.gestern.gringotts.api.TransactionResult;
+
+public class GringottsTaxedTransaction extends GringottsTransaction implements TaxedTransaction {
+	
+	/**
+	 * Taxes to apply to transaction.
+	 */
+	private final double taxes;
+	
+	@Override
+	public TransactionResult to(Account recipient) {
+		TransactionResult taxResult = from.remove(taxes);
+		if (taxResult != SUCCESS) return taxResult;
+		
+		TransactionResult result = super.to(recipient);
+		// undo taxing if transaction failed
+		if (result != SUCCESS) from.add(taxes);
+		return result; 
+	}
+
+	/**
+	 * Create taxed transaction, adding given amount of taxes to the given base transaction
+	 * @param base
+	 * @param taxes
+	 */
+	protected GringottsTaxedTransaction(GringottsTransaction base, double taxes) {
+		super(base);
+		this.taxes = taxes;
+    }
+
+	@Override
+    public TaxedTransaction collectedBy(Account taxCollector) {
+		throw new RuntimeException("tax collector account not yet implemented");
+    }
+
+	@Override
+    public double tax() {
+	    return taxes;
+    }
+
+}
---
+++ src/main/java/org/gestern/gringotts/api/impl/GringottsTransaction.java
@@ -0,0 +1,65 @@
+package org.gestern.gringotts.api.impl;
+
+import static org.gestern.gringotts.api.TransactionResult.ERROR;
+import static org.gestern.gringotts.api.TransactionResult.SUCCESS;
+
+import org.gestern.gringotts.Configuration;
+import org.gestern.gringotts.api.Account;
+import org.gestern.gringotts.api.TaxedTransaction;
+import org.gestern.gringotts.api.Transaction;
+import org.gestern.gringotts.api.TransactionResult;
+
+public class GringottsTransaction implements Transaction {
+
+	/** Account from which this transaction will withdraw money. */
+	protected final Account from;
+	
+	/** Base value of this transaction. */
+	protected double value;
+	
+	/**
+	 * Create Transaction based on another (copy ctor).
+	 * @param base
+	 */
+	protected GringottsTransaction(GringottsTransaction base) {
+		this.from = base.from;
+		this.value = base.value;
+	}
+	
+	/**
+	 * Create transaction with given source account and value.
+	 * @param from Account from which this transaction will withdraw money
+	 * @param amount base amount of this transaction
+	 */
+	GringottsTransaction(Account from, double value) {
+		this.from = from;
+		this.value = value;
+	}
+	
+	public TransactionResult to(Account to) {
+		if (value < 0) return ERROR;
+		
+		TransactionResult removed = from.remove(value);
+		if (removed == SUCCESS) {
+			TransactionResult added = to.add(value);
+			if (added != SUCCESS)
+				// adding failed, refund source
+				from.add(value);
+			
+			// returns success or reason add failed
+			return added;
+		}
+		// return reason remove failed
+        return removed;
+	}
+	
+	public TaxedTransaction withTaxes() {
+		
+		Configuration conf = Configuration.config;
+        
+        // FIXME take the damn business logic out of the command handler
+        double tax = conf.transactionTaxFlat + value * conf.transactionTaxRate;
+        return new GringottsTaxedTransaction(this, tax);
+	}
+	
+}
