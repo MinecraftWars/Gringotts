3cc956a318fe7960bfdfb24e8b2737ee8b8dc3e1
Justin Kaeser <jast@zno.de>
2013-02-20 02:19:10 +0100

    move factions listener to factions handler

--- src/main/java/org/gestern/gringotts/dependency/FactionsHandler.java
+++ src/main/java/org/gestern/gringotts/dependency/FactionsHandler.java
@@ -1,11 +1,17 @@
 package org.gestern.gringotts.dependency;
 
+import static org.gestern.gringotts.Permissions.createvault_faction;
+import static org.gestern.gringotts.dependency.Dependency.DEP;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.gestern.gringotts.Gringotts;
+import org.gestern.gringotts.accountholder.AccountHolder;
 import org.gestern.gringotts.accountholder.AccountHolderProvider;
 import org.gestern.gringotts.accountholder.FactionAccountHolder;
-import org.gestern.gringotts.event.FactionsListener;
+import org.gestern.gringotts.event.PlayerVaultCreationEvent;
 
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
@@ -85,5 +91,32 @@ public class FactionsHandler implements DependencyHandler, AccountHolderProvider
         
         return null;
     }
+
+	
+	public static class FactionsListener implements Listener {
+
+	    @EventHandler
+	    public void vaultCreated(PlayerVaultCreationEvent event) {
+	        // some listener already claimed this event
+	        if (event.isValid()) return;
+	        
+	        if (event.getType().equals("faction")) {
+	            Player player = event.getCause().getPlayer();
+	            if (!createvault_faction.allowed(player)) {
+	                player.sendMessage("You do not have permission to create a faction vault here.");
+	                return;
+	            }
+	            
+	            AccountHolder owner = DEP.factions.getFactionAccountHolder(player);
+	            if (owner==null) {
+	                player.sendMessage("Cannot create faction vault: You are not in a faction.");
+	                return;
+	            }
+	            
+	            event.setOwner(owner);
+	            event.setValid(true);
+	        } 
+	    }
+	}
 
 }
