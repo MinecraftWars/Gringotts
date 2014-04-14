0b33c97ee63fa5e83c2a0b3968f5e816e736c7d5
Justin Kaeser <jast@zno.de>
2014-04-14 13:58:37 +0200

    some style and design changes

--- src/main/java/org/gestern/gringotts/banking/Bank.java
+++ src/main/java/org/gestern/gringotts/banking/Bank.java
@@ -1,64 +1,57 @@
 package org.gestern.gringotts.banking;
 
+import java.util.Set;
 import java.util.HashSet;
-import org.bukkit.OfflinePlayer;
 
-public class Bank
-{
-  public final String name;
-  private HashSet<OfflinePlayer> managers;
-  
-  public Bank(String name)
-  {
-    this.name = name;
-    this.managers = new HashSet<OfflinePlayer>();
-  }
-  
-  public HashSet<OfflinePlayer> getManagers()
-  {
-    return this.managers;
-  }
-  
-  public boolean isManager(OfflinePlayer player)
-  {
-    return this.managers.contains(player);
-  }
-  
-  public boolean addManager(OfflinePlayer player)
-  {
-    this.managers.add(player);
-    return true;
-  }
-  
-  public boolean removeManager(OfflinePlayer player)
-  {
-    if (this.managers.contains(player))
-    {
-      this.managers.remove(player);
-      return true;
+import org.bukkit.OfflinePlayer;
+import org.gestern.gringotts.Gringotts;
+
+public class Bank {
+    public final String name;
+    private final Set<OfflinePlayer> managers = new HashSet<OfflinePlayer>();
+
+    public Bank(String name) {
+        this.name = name;
     }
-    return false;
-  }
-  
-  public FinanceHolder getReserve()
-  {
-    return new FinanceHolder(this, "bankr");
-  }
-  
-  public FinanceHolder getTrading()
-  {
-    return new FinanceHolder(this, "bankt");
-  }
-  
-  public BankAccountHolder getAccountHolder(String playerName)
-  {
-    if (playerName == null) {
-      return null;
+
+    public Set<OfflinePlayer> getManagers() {
+        return this.managers;
     }
-    OfflinePlayer player = Banker.dobby.getServer().getOfflinePlayer(playerName);
-    if (player == null) {
-      return null;
+
+    public boolean isManager(OfflinePlayer player) {
+        return this.managers.contains(player);
     }
-    return new BankAccountHolder(player, this);
-  }
+
+    public boolean addManager(OfflinePlayer player) {
+        this.managers.add(player);
+        return true;
+    }
+
+    public boolean removeManager(OfflinePlayer player) {
+        if (this.managers.contains(player)) {
+            this.managers.remove(player);
+            return true;
+        }
+        return false;
+    }
+
+    public FinanceHolder getReserve() {
+        return new FinanceHolder(this, "bankr");
+    }
+
+    public FinanceHolder getTrading() {
+        return new FinanceHolder(this, "bankt");
+    }
+
+    public BankAccountHolder getAccountHolder(String playerName) {
+        if (playerName == null) {
+            return null;
+        }
+        
+        OfflinePlayer player = Gringotts.G.getServer().getOfflinePlayer(playerName);
+        if (player == null) {
+            return null;
+        }
+        return new BankAccountHolder(player, this);
+    }
 }
--- src/main/java/org/gestern/gringotts/banking/Banker.java
+++ src/main/java/org/gestern/gringotts/banking/Banker.java
@@ -13,128 +13,109 @@ import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.gestern.gringotts.AccountChest;
 import org.gestern.gringotts.Accounting;
-import org.gestern.gringotts.Gringotts;
 import org.gestern.gringotts.accountholder.AccountHolder;
 import org.gestern.gringotts.accountholder.AccountHolderProvider;
 import org.gestern.gringotts.data.DAO;
 import org.gestern.gringotts.event.PlayerVaultCreationEvent;
 
+import static org.gestern.gringotts.Gringotts.G;
+
 public class Banker
-  implements AccountHolderProvider, Listener
-{
-  static Gringotts dobby;
-  static Logger log;
-  static Accounting cesare;
-  static DAO harold;
-  
-  public Banker(Gringotts plugin)
-  {
-    dobby = plugin;
-    log = dobby.getLogger();
-    cesare = dobby.accounting;
-    harold = dobby.dao;
-    
-    dobby.accountHolderFactory.registerAccountHolderProvider("bank", this);
-    dobby.accountHolderFactory.registerAccountHolderProvider("bankr", this);
-    dobby.accountHolderFactory.registerAccountHolderProvider("bankt", this);
-    dobby.accountHolderFactory.registerAccountHolderProvider("bankp", this);
-    dobby.getServer().getPluginManager().registerEvents(this, dobby);
-  }
-  
-  public AccountHolder getAccountHolder(String id)
-  {
-    if (id == null) {
-      return null;
+        implements AccountHolderProvider, Listener {
+    private final Logger log = G.getLogger();
+    private final Accounting accounting = G.accounting;
+    private final DAO dao = G.dao;
+
+    public Banker() {
+        G.accountHolderFactory.registerAccountHolderProvider("bank", this);
+        G.accountHolderFactory.registerAccountHolderProvider("bankr", this);
+        G.accountHolderFactory.registerAccountHolderProvider("bankt", this);
+        G.accountHolderFactory.registerAccountHolderProvider("bankp", this);
+        G.getServer().getPluginManager().registerEvents(this, G);
     }
-    String[] parsed = id.split("-");
-    if ((parsed.length < 2) || (parsed.length > 3)) {
-      return null;
+
+    public AccountHolder getAccountHolder(String id) {
+        if (id == null) return null;
+
+        String[] parsed = id.split("-");
+        if ((parsed.length < 2) || (parsed.length > 3)) return null;
+
+        String account = parsed[0];
+        Bank bank = getBank(parsed[1]);
+        String playerName = null;
+        if (parsed.length == 3) {
+            playerName = parsed[2];
+        }
+        if (account.equalsIgnoreCase("bankr")) {
+            return bank.getReserve();
+        }
+        if (account.equalsIgnoreCase("bankt")) {
+            return bank.getTrading();
+        }
+        if (account.equalsIgnoreCase("bankp")) {
+            return bank.getAccountHolder(playerName);
+        }
+        return null;
     }
-    String account = parsed[0];
-    Bank bank = getBank(parsed[1]);
-    String playerName = null;
-    if (parsed.length == 3) {
-      playerName = parsed[2];
+
+    public Bank getBank(String name) {
+        if (name.contains("-")) {
+            return null;
+        }
+        return new Bank(name);
     }
-    if (account.equalsIgnoreCase("bankr")) {
-      return bank.getReserve();
+
+    public List<String> listBanks() {
+        LinkedList<String> bankNames = new LinkedList<String>();
+        // TODO get the banks to list
+        return bankNames;
     }
-    if (account.equalsIgnoreCase("bankt")) {
-      return bank.getTrading();
+
+    @EventHandler
+    public void vaultCreation(PlayerVaultCreationEvent event) {
+        if (event.isValid()) {
+            return;
+        }
+        if (event.getType().equals("reserve")) {
+            Bank bank = getBank(event.getCause().getLine(2));
+            if (bank == null) {
+                event.getCause().getPlayer().sendMessage("You must specify a valid bank name on line 3.");
+            } else {
+                event.setOwner(bank.getReserve());
+            }
+            event.setValid(true);
+        }
+        if (event.getType().equals("teller")) {
+            Bank bank = getBank(event.getCause().getLine(2));
+            if (bank == null) {
+                event.getCause().getPlayer().sendMessage("You must specify a valid bank name on line 3.");
+            } else {
+                event.setOwner(bank.getTrading());
+            }
+            event.setValid(true);
+        }
     }
-    if (account.equalsIgnoreCase("bankp")) {
-      return bank.getAccountHolder(playerName);
+
+    public void markTeller(Block tellerBlock) {
+        log.info("Marking blocktype " + tellerBlock.getState().getTypeId());
+        tellerBlock.setMetadata("GringottsTeller", new FixedMetadataValue(G, true));
     }
-    return null;
-  }
-  
-  public Bank getBank(String name)
-  {
-    if (name.contains("-")) {
-      return null;
+
+    private void unmarkTeller(Block tellerBlock) {
+        tellerBlock.removeMetadata("GringottsTeller", G);
     }
-    return new Bank(name);
-  }
-  
-  public List<String> listBanks()
-  {
-    LinkedList<String> bankNames = new LinkedList<String>();
-    return bankNames;
-  }
-  
-  @EventHandler
-  public void vaultCreation(PlayerVaultCreationEvent event)
-  {
-    if (event.isValid()) {
-      return;
+
+    private Set<AccountChest> getTellerChests() {
+        return null;
     }
-    if (event.getType().equals("reserve"))
-    {
-      Bank bank = getBank(event.getCause().getLine(2));
-      if (bank == null) {
-        event.getCause().getPlayer().sendMessage("You must specify a valid bank name on line 3.");
-      } else {
-        event.setOwner(bank.getReserve());
-      }
-      event.setValid(true);
+
+    @EventHandler
+    public void accessTeller(InventoryOpenEvent event) {
+        if ((event.getInventory().getHolder() instanceof Chest)) {
+            Chest chest = (Chest) event.getInventory().getHolder();
+            if (chest.hasMetadata("GringottsTeller")) {
+                log.info("meep");
+            }
+        }
     }
-    if (event.getType().equals("teller"))
-    {
-      Bank bank = getBank(event.getCause().getLine(2));
-      if (bank == null) {
-        event.getCause().getPlayer().sendMessage("You must specify a valid bank name on line 3.");
-      } else {
-        event.setOwner(bank.getTrading());
-      }
-      event.setValid(true);
-    }
-  }
-  
-  public void markTeller(Block tellerBlock)
-  {
-    log.info("Marking blocktype " + tellerBlock.getState().getTypeId());
-    tellerBlock.setMetadata("GringottsTeller", new FixedMetadataValue(dobby, Boolean.valueOf(true)));
-  }
-  
-  private void unmarkTeller(Block tellerBlock)
-  {
-    tellerBlock.removeMetadata("GringottsTeller", dobby);
-  }
-  
-  private Set<AccountChest> getTellerChests()
-  {
-    return null;
-  }
-  
-  @EventHandler
-  public void accessTeller(InventoryOpenEvent event)
-  {
-    if ((event.getInventory().getHolder() instanceof Chest))
-    {
-      Chest chest = (Chest)event.getInventory().getHolder();
-      if (chest.hasMetadata("GringottsTeller")) {
-        log.info("meep");
-      }
-    }
-  }
 }
