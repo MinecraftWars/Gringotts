6ddb1a58cd4f47da7c6800729422dfaddbc5bb56
Brendan Grieve <brendan.grieve@zepli.com.au>
2018-12-17 19:36:56 +0800

    Implement support for WorldGuard 7.0.0 only dropping support for pre 1.13
    Changes:
      * Use WorldGuard 7.0.0 API

--- src/main/java/org/gestern/gringotts/dependency/WorldGuardHandler.java
+++ src/main/java/org/gestern/gringotts/dependency/WorldGuardHandler.java
@@ -1,7 +1,9 @@
 package org.gestern.gringotts.dependency;
 
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.domains.DefaultDomain;
+import com.sk89q.worldguard.internal.platform.WorldGuardPlatform;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Bukkit;
@@ -47,7 +49,7 @@ public class WorldGuardHandler implements DependencyHandler, AccountHolderProvid
     @Override
     public WorldGuardAccountHolder getAccountHolder(String id) {
         // FIXME use something more robust than - as world-id delimiter
-        // try explicit world+id first 
+        // try explicit world+id first
         String[] parts = id.split("-", 2);
         if (parts.length == 2) {
             WorldGuardAccountHolder wgah = getAccountHolder(parts[0], parts[1]);
@@ -58,10 +60,11 @@ public class WorldGuardHandler implements DependencyHandler, AccountHolderProvid
         }
 
         // try bare id in all worlds
+        WorldGuardPlatform worldguardPlatform = WorldGuard.getInstance().getPlatform();
         for (World world : Bukkit.getWorlds()) {
-            RegionManager worldManager = plugin.getRegionManager(world);
+            RegionManager worldManager = worldguardPlatform.getRegionContainer().get(worldguardPlatform.getWorldByName(world.getName()));
 
-            if (worldManager.hasRegion(id)) {
+            if (worldManager != null && worldManager.hasRegion(id)) {
                 ProtectedRegion region = worldManager.getRegion(id);
 
                 return new WorldGuardAccountHolder(world.getName(), region);
@@ -85,14 +88,15 @@ public class WorldGuardHandler implements DependencyHandler, AccountHolderProvid
             return null;
         }
 
-        RegionManager manager = plugin.getRegionManager(w);
+        WorldGuardPlatform worldguardPlatform = WorldGuard.getInstance().getPlatform();
+        RegionManager worldManager = worldguardPlatform.getRegionContainer().get(worldguardPlatform.getWorldByName(w.getName()));
 
-        if (manager == null) {
+        if (worldManager == null) {
             return null;
         }
 
-        if (manager.hasRegion(id)) {
-            ProtectedRegion region = manager.getRegion(id);
+        if (worldManager.hasRegion(id)) {
+            ProtectedRegion region = worldManager.getRegion(id);
 
             return new WorldGuardAccountHolder(world, region);
         }
